a95f1ce4a01762f3ca54a4d694601ec0e3e6feeb
Abhay Saxena <abhaysaxena8888@gmail.com>
2026-02-19 14:08:21 +0530

    changes 19/02

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -59,7 +59,7 @@ export default function Home() {
           <WhatWeDo />
           <Competitions />
           <Workshops />
-          <Judges />
+          {/* <Judges /> */}
           <Speakers />
         </Suspense>
       </main>
